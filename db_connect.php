359d4f65364ba3a34529401c59e3ce2155736344
Ardilan <dilancorr@gmail.com>
2023-03-12 21:25:04 -0500

    pruebas

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,3 @@
 <?php
 
-$conn = new mysqli('localhost', 'root', 'HaikoSushi', '9.min.pa1') or die("Could not connect to mysql" . mysqli_error($con));
+$conn = new mysqli('localhost', 'root', '', 'tareas') or die("Could not connect to mysql" . mysqli_error($con));
